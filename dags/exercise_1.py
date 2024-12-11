3280ac87890eed446bf34f56ab34e20754f1504c
Travis Dent <travis.dent@xebia.com>
2024-12-11 14:18:31 +0100

    Exercise 1

---
+++ dags/exercise_1.py
@@ -0,0 +1,21 @@
+import pendulum
+from airflow.models import DAG
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="01_Rocket_launch_structure",
+    start_date=pendulum.today("UTC").add(days=-90),
+    schedule="@daily",
+    catchup=True,
+) as dag:
+    procure_rocket_material = EmptyOperator(task_id="procure_rocket_material")
+
+    building = [EmptyOperator(task_id=f"build_stage_{i}") for i in range(1, 4)]
+
+    procure_fuel = EmptyOperator(task_id="procure_fuel")
+
+    launch_rocket = EmptyOperator(task_id="launch_rocket")
+
+    procure_rocket_material >> building
+    procure_fuel >> building[-1]
+    building >> launch_rocket
