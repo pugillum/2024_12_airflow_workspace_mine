55dff3ba1b91a98044c8574d5fc45009b650447d
Travis Dent <travis.dent@xebia.com>
2024-12-13 14:21:16 +0100

    Big exercise update

---
+++ exercises/solutions/capstone_improvements.py
@@ -0,0 +1,167 @@
+from datetime import datetime
+import json
+import os
+
+from airflow import DAG
+from airflow.exceptions import AirflowSkipException
+from airflow.operators.python import PythonOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.google.cloud.transfers.local_to_gcs import (
+    LocalFilesystemToGCSOperator,
+)
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryCreateEmptyDatasetOperator,
+)
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import (
+    GCSToBigQueryOperator,
+)
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.google.cloud.transfers.bigquery_to_postgres import (
+    BigQueryToPostgresOperator,
+)
+
+import pandas as pd
+
+GOOGLE_CLOUD_CONN_ID = "google_cloud_default"
+USER_NAME = "travis"
+PROJECT_NAME = "airflow-training-20241210"
+
+
+def is_launch_today(task_instance, **context):
+    response = task_instance.xcom_pull(task_ids="extract_launch")
+    response_dict = json.loads(response)
+    if response_dict["count"] == 0:
+        raise AirflowSkipException(f"No data found on day {context['ds']}")
+
+
+def extract_relevant_data(x: dict):
+    return {
+        "id": x.get("id"),
+        "name": x.get("name"),
+        "status": x.get("status").get("abbrev"),
+        "country_code": x.get("pad").get("country").get("alpha_2_code"),
+        "service_provider_name": x.get("launch_service_provider").get("name"),
+        "service_provider_type": x.get("launch_service_provider")
+        .get("type")
+        .get("name"),
+    }
+
+
+def convert_to_parquet(task_instance, **context):
+    response = task_instance.xcom_pull(task_ids="extract_launch")
+    response_dict = json.loads(response)
+    response_results = response_dict["results"]
+
+    # create /tmp/lauches directory if it does not exist
+    os.makedirs("/tmp/launches", exist_ok=True)
+
+    (
+        pd.DataFrame([extract_relevant_data(i) for i in response_results]).to_parquet(
+            path=f"/tmp/launches/{context['ds']}.parquet"
+        )
+    )
+
+    print(f"Writen to /tmp/launches/{context['ds']}.parquet")
+    print("Files in launches:", os.listdir("/tmp/launches"))
+
+
+with DAG(
+    dag_id="capstone_project_copy_into_postgres",
+    start_date=datetime(2023, 12, 11),
+    end_date=datetime(2023, 12, 16),
+    schedule="@daily",
+):
+    is_api_available = HttpSensor(
+        task_id="is_api_available", http_conn_id="thespacedevs_dev", endpoint=""
+    )
+
+    extract_launch = SimpleHttpOperator(  # This can be HttpOperator
+        task_id="extract_launch",
+        http_conn_id="thespacedevs_dev",
+        endpoint="",
+        method="GET",
+        data={
+            "window_start__gte": "{{data_interval_start|ds}}T00:00:00Z",  # use of data_interval - check if ts can be used and remove the T00:00:00Z
+            "window_end__lt": "{{data_interval_end|ds}}T00:00:00Z",
+        },
+        log_response=True,
+    )
+
+    is_there_launch_today = PythonOperator(
+        task_id="is_there_launch_today",
+        python_callable=is_launch_today,
+    )
+
+    convert_to_parquet = PythonOperator(
+        task_id="convert_to_parquet",
+        python_callable=convert_to_parquet,
+    )
+
+    cloud_file_storage = LocalFilesystemToGCSOperator(
+        gcp_conn_id=GOOGLE_CLOUD_CONN_ID,
+        task_id="cloud_file_storage",
+        src="/tmp/launches/{{ds}}.parquet",
+        dst=f"{USER_NAME}" + "/launches/{{ds}}.parquet",
+        bucket=PROJECT_NAME,
+    )
+
+    create_new_BQ_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id="new_dataset_creator",
+        dataset_id=f"{USER_NAME}_dataset",
+        project_id=PROJECT_NAME,
+        gcp_conn_id=GOOGLE_CLOUD_CONN_ID,
+    )
+
+    load_to_bigquery = GCSToBigQueryOperator(
+        task_id="load_to_bigquery",
+        bucket=PROJECT_NAME,
+        source_objects=[f"{USER_NAME}" + "/launches/{{ds}}.parquet"],
+        destination_project_dataset_table=f"{PROJECT_NAME}.{USER_NAME}_dataset.rocket_launches",
+        source_format="parquet",
+        write_disposition="WRITE_APPEND",
+        autodetect=True,
+        gcp_conn_id=GOOGLE_CLOUD_CONN_ID,
+    )
+
+    create_postgres_table = PostgresOperator(
+        task_id="create_postgres_table",
+        postgres_conn_id="postgres",
+        sql="""
+            CREATE TABLE IF NOT EXISTS rocket_launches (
+                id TEXT NOT NULL,
+                name TEXT NOT NULL,
+                status TEXT NOT NULL,
+                country TEXT NOT NULL,
+                launch_service_provider_name TEXT NOT NULL,
+                launch_service_provider_type TEXT 
+            );
+        """,
+    )
+
+    """
+    Remember! To see the data in postgres db:
+        docker ps
+        docker exec -it <db-postgres name> /bin/bash
+        psql -U airflow
+        \dt # see tables
+        SELECT * FROM rocket_launches;
+    """
+
+    store_launch_in_postgres_db = BigQueryToPostgresOperator(
+        task_id="insert_into_postgres_table",
+        dataset_table=f"{USER_NAME}_dataset." + "rocket_launches",
+        target_table_name="rocket_launches",
+        postgres_conn_id="postgres",
+    )
+
+    is_api_available >> extract_launch >> is_there_launch_today >> convert_to_parquet
+
+    (
+        convert_to_parquet
+        >> cloud_file_storage
+        >> create_new_BQ_dataset
+        >> load_to_bigquery
+        >> create_postgres_table
+        >> store_launch_in_postgres_db
+    )
--- exercises/solutions/exercise_3.py
+++ exercises/solutions/exercise_3.py
@@ -1,33 +1,65 @@
-from datetime import datetime, timedelta
-from airflow.models import DAG
+import json
+from pathlib import Path
+
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+from airflow.models import DAG
+import airflow.utils.dates
+import requests
 
-
-def print_message(**context):
-    # print(f"This DAG run was for period {context["data_interval_start"]} to {context["data_interval_end"]}")
-    data_interval_start = context["data_interval_start"]
-    data_interval_end = context["data_interval_end"]
-    print(
-        f"🔥 This DAG run was for period {data_interval_start} to {data_interval_end}"
-    )
-    print(f"🔥 This DAG run was triggered on {context['dag_run'].queued_at}")
-
+# Switch to the second URL (the dev endpoint) if you get rate-limited.
+# The data will be old, but at least it will work.
+API_URL = "https://ll.thespacedevs.com/2.2.0/launch"
+# API_URL = "https://lldev.thespacedevs.com/2.2.0/launch"
 
 with DAG(
-    dag_id="exercise_3",
-    description="This DAG will print 'Hello' & 'World'.",
-    start_date=datetime.now() - timedelta(days=2),
-    schedule="@daily",
-):
-    echo_task = BashOperator(
-        task_id="echo_task",
-        bash_command='echo "🔥 Task ID: {{ task.task_id }}, DAG ID: {{ dag.dag_id }}"',
+    dag_id="exercise_templating",
+    start_date=airflow.utils.dates.days_ago(7),
+    schedule_interval="@daily",
+) as dag:
+
+    def _download_launches(templates_dict, **_):
+        output_path = Path(templates_dict["output_path"])
+
+        response = requests.get(
+            API_URL,
+            params={
+                "window_start__gte": templates_dict["window_start"],
+                "window_end__lt": templates_dict["window_end"],
+            },
+        )
+        response.raise_for_status()
+
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        with output_path.open("w") as file_:
+            json.dump(response.json(), file_)
+
+    def _print_launch_count(templates_dict, **_):
+        input_path = templates_dict["input_path"]
+
+        with Path(input_path).open() as file_:
+            launches = json.load(file_)
+
+        print(f"""Counted {launches["count"]} launches from {input_path}""")
+
+    print_date = BashOperator(
+        task_id="print_date", bash_command="echo {{ execution_date }}"
     )
 
-    date_task = PythonOperator(
-        task_id="date_task",
-        python_callable=print_message,
+    download_launches = PythonOperator(
+        task_id="download_launches",
+        python_callable=_download_launches,
+        templates_dict={
+            "output_path": "/tmp/launches/{{ds}}.json",
+            "window_start": "{{ds}}T00:00:00Z",
+            "window_end": "{{next_ds}}T00:00:00Z",
+        },
     )
 
-    echo_task >> date_task
+    check_for_launches = PythonOperator(
+        task_id="check_for_launches",
+        python_callable=_print_launch_count,
+        templates_dict={"input_path": "/tmp/launches/{{ds}}.json"},
+    )
+
+    print_date >> download_launches >> check_for_launches
