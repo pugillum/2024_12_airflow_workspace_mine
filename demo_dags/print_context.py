283ce9ba12b055d07ed3727b3de624979e5cf95a
Travis Dent <travis.dent@xebia.com>
2024-12-13 11:37:40 +0100

    Adjust print_context

--- demo_dags/print_context.py
+++ demo_dags/print_context.py
@@ -1,7 +1,4 @@
-from datetime import datetime, timedelta
-
 from airflow.models import DAG
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 from pprint import pprint
@@ -11,19 +8,8 @@ def print_context_func(**context):
     pprint(context)
 
 
-with DAG(
-    dag_id="print_context",
-    start_date=datetime.now() - timedelta(days=14),
-    description="This DAG will print 'Hello' & 'World'.",
-    schedule="@daily",
-):
-    hello = BashOperator(task_id="hello", bash_command="echo 'hello'")
-
-    world = PythonOperator(task_id="world", python_callable=lambda: print("world"))
-
+with DAG(dag_id="print_context", schedule=None, is_paused_upon_creation=False):
     print_context = PythonOperator(
         task_id="print_context",
         python_callable=print_context_func,
     )
-
-    hello >> world >> print_context
