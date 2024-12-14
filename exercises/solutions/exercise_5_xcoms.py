66bfcbe016b09bb0232795876b44348121329a09
Travis Dent <travis.dent@xebia.com>
2024-12-14 16:36:01 +0100

    Xcoms

--- exercises/solutions/exercise_5_xcoms.py
+++ exercises/solutions/exercise_5_xcoms.py
@@ -1,6 +1,3 @@
-import json
-from pathlib import Path
-
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.models import DAG
@@ -10,14 +7,14 @@ import requests
 API_URL = "https://lldev.thespacedevs.com/2.2.0/launch"
 
 with DAG(
-    dag_id="exercise_templating",
+    dag_id="exercise_5_xcoms",
     start_date=airflow.utils.dates.days_ago(7),
     schedule_interval="@daily",
+    tags=["exercise"],
 ) as dag:
 
-    def _download_launches(**context):
+    def _download_launches(task_instance, **context):
         templates_dict = context["templates_dict"]
-        output_path = Path(templates_dict["output_path"])
 
         response = requests.get(
             API_URL,
@@ -27,18 +24,18 @@ with DAG(
             },
         )
         response.raise_for_status()
+        return response.json()
 
-        output_path.parent.mkdir(parents=True, exist_ok=True)
-        with output_path.open("w") as file_:
-            json.dump(response.json(), file_)
+    def _print_launch_count(task_instance, **_):
 
-    def _print_launch_count(**context):
-        input_path = context["templates_dict"]["input_path"]
+        launches = task_instance.xcom_pull(task_ids="download_launches")
 
-        with Path(input_path).open() as file_:
-            launches = json.load(file_)
+        # input_path = context["templates_dict"]["input_path"]
 
-        print(f"""Counted {launches["count"]} launches from {input_path}""")
+        # with Path(input_path).open() as file_:
+        #     launches = json.load(file_)
+
+        print(f"""Counted {launches["count"]} launches""")
 
     print_date = BashOperator(
         task_id="print_date", bash_command="echo {{ execution_date }}"
@@ -48,7 +45,6 @@ with DAG(
         task_id="download_launches",
         python_callable=_download_launches,
         templates_dict={
-            "output_path": "/tmp/launches/{{ds}}.json",
             "window_start": "{{ds}}T00:00:00Z",
             "window_end": "{{next_ds}}T00:00:00Z",
         },
